092720ca9e8a6cc2632c7f7f36fee1b6bc1709c2
Kevin Shah <kevin.shah@live.com>
2014-06-05 23:07:03 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Kevin Shah on 6/5/14.
+//  Copyright (c) 2014 KS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
